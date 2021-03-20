c8eb22f9af58333fdbce004edb04ff7e346a7be7
edchron <63876304+edchron@users.noreply.github.com>
2021-03-21 00:22:10 +0700

    Update KeyPad.h

--- KeyPad.h
+++ KeyPad.h
@@ -1,14 +1,14 @@
 #ifndef _KEYPAD_H_
 #define _KEYPAD_H_
 #include<Arduino.h>
-#define buatKeymap(x) ((char*)x)
+#define MakeKeymap(x) ((char*)x)
 
 class KeyPad{
 	public:
 	KeyPad(char *_keys, byte _ROWS, byte _COLS, byte *_rowPins, byte *_colPins);
 	
 	void begin(char *_keys);
-	unsigned char cek_kolom();
+	unsigned char check_column();
 	unsigned char activate_row(unsigned char row_th);
 	unsigned char ReadKeypad();
 
